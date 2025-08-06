9aae4e324f1454604d417cb113fe75f74219dcf4
rodybothe <rody.bothe@dyflexis.com>
2025-08-06 11:48:26 +0200

    update k6

--- aws-jenkins-observability/k6/test_api.js
+++ aws-jenkins-observability/k6/test_api.js
@@ -2,9 +2,8 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export default function () {
-    const baseUrl = 'http://localhost:8000';
+    const baseUrl = 'http://host.docker.internal:8000';
 
-    // Step 1: Create an item
     const payload = JSON.stringify({
         name: "TestItem",
         description: "Created via k6"
@@ -21,12 +20,11 @@ export default function () {
     const itemId = createRes.json().id;
     console.log(`Created item with ID: ${itemId}`);
 
-    // Step 2: Delete the item
     const deleteRes = http.del(`${baseUrl}/items/${itemId}`);
 
     check(deleteRes, {
         'deleted item successfully': (r) => r.status === 200,
     });
 
-    sleep(1); // optional
+    sleep(1);
 }
